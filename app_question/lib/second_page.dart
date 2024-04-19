d3b4e21f209f7267fddd209511bf6147a75354fa
Azkabanrito1 <sulaimonhassan500@gmail.com>
2024-04-19 10:43:32 +0100

    first

--- app_question/lib/second_page.dart
+++ app_question/lib/second_page.dart
@@ -12,7 +12,7 @@ class SecondPage extends StatelessWidget {
         gradient: LinearGradient(
           colors: [
             Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 88),
+            Color.fromARGB(255, 45, 7, 98),
           ],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
